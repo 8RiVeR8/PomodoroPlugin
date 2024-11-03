7baa01fafc507f10233d483d3ba330d367da4653
Maksymilian Rzeczycki <maks.rzeczycki@gmail.com>
2024-11-03 20:02:00 +0100

    Test pomodoro implementation

--- src/main/java/org/example/pomodoroplugin/PomodoroTimerTest.java
+++
@@ -1,6 +0,0 @@
-package org.example.pomodoroplugin;
-import org.junit.jupiter.api.Test;
-
-class PomodoroTimerTest {
-
-}
---
+++ src/test/java/org/example/pomodoroplugin/PomodoroTimerTest.java
@@ -0,0 +1,11 @@
+package org.example.pomodoroplugin;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PomodoroTimerTest extends TestCase {
+    @Test
+    public void shouldCreateContentPane(){
+        var pomodoroTimer = new PomodoroTimer();
+    }
+}
